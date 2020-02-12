750c2e3a62a9ba80ee477e5f4bb2d7e7db54bf88
Craig Send 🐘 <craig.send@bain.com>
2020-02-12 20:04:54 +0100

    Add eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,97 +1,108 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {Button, Container, Grid} from "@material-ui/core";
-import {hash} from "./spotify/authenicate";
-import SpotifyWebApi from "spotify-web-api-js";
-import SongTitle from "./components/SongTitle";
-
+import { Button, Container, Grid } from '@material-ui/core';
+import { hash } from './spotify/authenicate';
+import SpotifyWebApi from 'spotify-web-api-js';
+import SongTitle from './components/SongTitle';
 
 const App = () => {
-    const authorizationEndpoint = "https://accounts.spotify.com/authorize";
-    const clientId = "664624fb8bb640fb8b69b4d3e6a18d12";
-    const scopes = ["user-read-private", "user-read-currently-playing", "user-read-playback-state", "user-read-recently-played"];
-    const responseType = "token";
-    const redirectUri = "http://localhost:7000";
+  const authorizationEndpoint = 'https://accounts.spotify.com/authorize';
+  const clientId = '664624fb8bb640fb8b69b4d3e6a18d12';
+  const scopes = [
+    'user-read-private',
+    'user-read-currently-playing',
+    'user-read-playback-state',
+    'user-read-recently-played',
+  ];
+  const responseType = 'token';
+  const redirectUri = 'http://localhost:7000';
 
-    const [token, setToken] = useState(hash.access_token);
-    const [currentlyPlaying, setCurrentlyPlaying] = useState('');
-    const [recentlyPlayed, setRecentlyPlayed] = useState([]);
+  const token = hash.access_token;
 
-    const spotifyApi = new SpotifyWebApi();
+  const [currentlyPlaying, setCurrentlyPlaying] = useState('');
+  const [recentlyPlayed, setRecentlyPlayed] = useState([]);
 
-    const getAuthUrl = () => {
-        return `${authorizationEndpoint}?client_id=${clientId}&response_type=${responseType}&scope=${scopes.join('%20')}&redirect_uri=${redirectUri}`;
-    };
+  const spotifyApi = new SpotifyWebApi();
 
-    const getCurrentlyPlaying = () => {
-        spotifyApi.setAccessToken(token);
-        spotifyApi.getMyCurrentPlayingTrack()
-            .then(res => {
-                setCurrentlyPlaying(res);
-            });
-    };
-
-    const getRecentlyPlayed = () => {
-        spotifyApi.setAccessToken(token);
-        spotifyApi.getMyRecentlyPlayedTracks({
-            after: Date.now() - (3600000 * 24)
-        })
-            .then(res => {
-                console.log(`recently played (${res.items.length})`, res.items);
-                setRecentlyPlayed(res.items);
-            });
-    };
-
-    useEffect(() => {
-        getCurrentlyPlaying(token, setCurrentlyPlaying);
-    }, [token]);
-
-    return (
-      <Container fixed>
-          <Grid container spacing={2}>
-              <Grid item xs={12}>
-                  <Grid container direction="column" justify="center" spacing={4}>
-                      <Grid item>
-                          <p>
-                              User is {!token && `not`} logged in.
-                          </p>
-                      </Grid>
-                      <Grid item>
-                          <Button href={getAuthUrl()}>Click to log in</Button>
-                      </Grid>
-                      {token && (
-                          <Grid item>
-                              <Button onClick={getCurrentlyPlaying}>Get currently playing song</Button>
-
-                              {currentlyPlaying && (
-                                  <Grid>
-                                      <h4>Now playing:</h4>
-                                      <SongTitle song={currentlyPlaying}/>
-                                      <img src={currentlyPlaying.item.album.images[0].url}/>
-                                  </Grid>
-                              )}
-                          </Grid>
-                      )}
-
-                      {token && (
-                          <Grid item>
-                              <Button onClick={getRecentlyPlayed}>Get recently played tracks</Button>
-
-                              {recentlyPlayed.length > 0 && (
-                                  <Grid>
-                                      <h4>Recently played:</h4>
-                                      {recentlyPlayed.map(item => (
-                                          <p>{item.track.name} by {item.track.artists[0].name}</p>
-                                      ))}
-                                  </Grid>
-                              )}
-                          </Grid>
-                      )}
-                  </Grid>
-              </Grid>
-          </Grid>
-      </Container>
-    );
+  const getAuthUrl = () => {
+    return `${authorizationEndpoint}?client_id=${clientId}&response_type=${responseType}&scope=${scopes.join(
+      '%20'
+    )}&redirect_uri=${redirectUri}`;
   };
+
+  const getCurrentlyPlaying = () => {
+    spotifyApi.setAccessToken(token);
+    spotifyApi.getMyCurrentPlayingTrack().then(res => {
+      setCurrentlyPlaying(res);
+    });
+  };
+
+  const getRecentlyPlayed = () => {
+    spotifyApi.setAccessToken(token);
+    spotifyApi
+      .getMyRecentlyPlayedTracks({
+        after: Date.now() - 3600000 * 24,
+      })
+      .then(res => {
+        console.log(`recently played (${res.items.length})`, res.items);
+        setRecentlyPlayed(res.items);
+      });
+  };
+
+  useEffect(() => {
+    getCurrentlyPlaying(token, setCurrentlyPlaying);
+  }, [token]);
+
+  return (
+    <Container fixed>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container direction="column" justify="center" spacing={4}>
+            <Grid item>
+              <p>User is {!token && 'not'} logged in.</p>
+            </Grid>
+            <Grid item>
+              <Button href={getAuthUrl()}>Click to log in</Button>
+            </Grid>
+            {token && (
+              <Grid item>
+                <Button onClick={getCurrentlyPlaying}>
+                  Get currently playing song
+                </Button>
+
+                {currentlyPlaying && (
+                  <Grid>
+                    <h4>Now playing:</h4>
+                    <SongTitle song={currentlyPlaying} />
+                    <img src={currentlyPlaying.item.album.images[0].url} />
+                  </Grid>
+                )}
+              </Grid>
+            )}
+
+            {token && (
+              <Grid item>
+                <Button onClick={getRecentlyPlayed}>
+                  Get recently played tracks
+                </Button>
+
+                {recentlyPlayed.length > 0 && (
+                  <Grid>
+                    <h4>Recently played:</h4>
+                    {recentlyPlayed.map(item => (
+                      <p>
+                        {item.track.name} by {item.track.artists[0].name}
+                      </p>
+                    ))}
+                  </Grid>
+                )}
+              </Grid>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,46 @@ const DEV_ENTRY_POINT = 'index.js';
 const INDEX_HTML = 'index.html';
 
 const base = {
-    entry: path.resolve(SRC_FOLDER, DEV_ENTRY_POINT),
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                ],
-            },
-        ]
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, SRC_FOLDER),
-        compress: true,
-        hot: true,
-        port: 7000,
-    },
-    plugins: [
-        new HtmlWebpackPLugin({
-            template: path.resolve(SRC_FOLDER, INDEX_HTML),
-        }),
-    ]
+  entry: path.resolve(SRC_FOLDER, DEV_ENTRY_POINT),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, SRC_FOLDER),
+    compress: true,
+    hot: true,
+    port: 7000,
+  },
+  plugins: [
+    new HtmlWebpackPLugin({
+      template: path.resolve(SRC_FOLDER, INDEX_HTML),
+    }),
+  ],
 };
 
 module.exports = base;
